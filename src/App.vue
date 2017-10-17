470e180e767fba41e1d9e91b0d4a979ffeee7765
uidevperson <uidevthing@gmail.com>
2017-10-17 21:03:15 +0100

    counter component test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-  
+    <counter></counter>
   </div>
 </template>
 
 <script>
+import Counter from './components/Counter.vue';
+
 export default {
   name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+  components: { Counter }
+  // data () {
+  //   return {
+  //     msg: 'Welcome to Your Vue.js App'
+  //   }
+  // }
 }
 </script>
 
---
+++ src/components/Counter.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<h1>this count is test {{count}}</h1>
+		<button @click='count+=10'>increment</button>
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+		data(){
+			return {
+				count: 0
+			};
+		}
+	}
+</script>
